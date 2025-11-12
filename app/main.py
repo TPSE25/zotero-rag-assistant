b42070c1ec1c3f26e1925477abca89fa95001dce
David Scandurra <31861387+SplotyCode@users.noreply.github.com>
2025-11-12 19:41:18 +0100

    Configure nginx as gateway

--- app/main.py
+++ app/main.py
@@ -6,14 +6,10 @@ app = FastAPI(title="Dummy REST API", version="0.1.0")
 class EchoIn(BaseModel):
     message: str
 
-@app.get("/health")
+@app.get("/api/health")
 async def health():
     return {"status": "ok"}
 
-@app.post("/echo")
+@app.post("/api/echo")
 async def echo(body: EchoIn):
     return {"you_said": body.message}
-
-@app.get("/")
-async def root():
-    return {"hello": "world"}

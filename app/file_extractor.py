4c55fdedf0f597bb65aff33a696178de496247a7
ruixuange <geruixuan707@gmail.com>
2025-12-07 13:48:04 +0100

    file_extractor_2

--- app/file_extractor.py
+++ app/file_extractor.py
@@ -1,9 +1,49 @@
 import zipfile
 import os
-from pypdf import PdfReader
+import mimetypes
+from typing import Dict, List, Union
+import logging
+import pdfplumber
+logger = logging.getLogger(__name__)
+
 def extract_from_pdf(file_path: str) -> str:
-	reader = PdfReader(file_path)
-	text = ""
-	for page in reader.pages:
-		text += page.extract_text()
-	return text
+    try:
+        if not os.path.exists(file_path):
+            logger.error(f"PDF not found: {file_path}")
+            return ""
+
+        with pdfplumber.open(file_path) as pdf:
+            text = "\n".join(
+                (page.extract_text() or "") for page in pdf.pages
+        )
+        return text
+
+    except Exception as e:
+        logger.error(f"Failed to extract PDF '{file_path}': {e}", exc_info=True)
+        return ""
+
+    
+def extract_from_zip(file_path:str)->Dict[str, str]:
+    texts = {}
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        for file_info in zip_ref.infolist():
+            if not file_info.is_dir():
+                with zip_ref.open(file_info) as file:
+                    try:
+                        content = file.read().decode('utf-8')
+                        texts[file_info.filename] = content
+                    except UnicodeDecodeError:
+                        continue
+    return texts
+
+def extract_auto(file_path: str) -> Dict[str, str]:
+    mime_type, _ = mimetypes.guess_type(file_path)
+    if mime_type:
+        if mime_type == 'application/pdf':
+            text = extract_from_pdf(file_path)
+            return {os.path.basename(file_path): text}
+        elif mime_type == 'application/zip':
+            return extract_from_zip(file_path)
+        else:
+            logging.warning(f"Unsupported MIME type: {mime_type}")
+            return {}

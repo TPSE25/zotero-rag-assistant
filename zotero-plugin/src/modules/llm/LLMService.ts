c62102fce58ca9e29ab391cde99196a6ee5326f0
Utku Saglam <114105533+utkussaglam@users.noreply.github.com>
2026-01-11 14:38:55 +0100

    Delete zotero-plugin directory

--- zotero-plugin/src/modules/llm/LLMService.ts
+++
@@ -1,8 +0,0 @@
-export interface ChatMessage {
-  role: "user" | "assistant";
-  content: string;
-}
-
-export interface LLMService {
-  sendMessage(messages: ChatMessage[]): Promise<ChatMessage>;
-}
